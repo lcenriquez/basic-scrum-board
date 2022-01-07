da84522d1d2ef7f0f76a1df3552061fc2c7b1d2f
lcenriquez <luiscarlos_enriquez@me.com>
2022-01-06 22:11:20 -0600

    Create task functionality

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,11 +1,23 @@
+import { useState } from 'react';
 import style from './Board.module.css';
 import Task from './Task.tsx';
 
-export default function Board({ name, tasks }) {
+export default function Board({ id, name, tasks, createTask, dismiss }) {
+  const [ create, setCreate ] = useState(false);
+
+  function newTask() {setCreate(true)}
+  function cancelNewTask() {setCreate(false)}
+  function handleSumbit(input) {
+    createTask(input);
+    setCreate(false);
+  }
+
   return (
     <div className={style.boardContainer}>
       <h1 className={style.boardTitle}>{name}</h1>
-      { tasks?.map((task, id) => <Task key={id} {...task} />) }
+      { tasks?.map((task, index) => task.boardId===id ? <Task key={index} action="show" {...task} /> : null) }
+      { create ? <Task action="create" create={handleSumbit} boardId={id} /> : null }
+      { create ? <p className={style.addTask} onClick={cancelNewTask}>Close</p> : <p className={style.addTask} onClick={newTask}>Add new task</p> }
     </div>
   )
 };
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,11 +1,31 @@
+import { useState } from 'react';
 import style from './Task.module.css';
 
-export default function Task({ name, description, assignedTo }) {
-  return (
-    <div className={style.taskContainer}>
-      <h2 className={style.title}>{name}</h2>
-      <p>{description}</p>
-      <small>Assigned to: {assignedTo}</small>
-    </div>
-  )
+export default function Task({ boardId, title, description, action, assignedTo, create }) {
+  const [ input, setInput ] = useState({boardId: boardId, title: '', description: ''});
+
+  function handleChange(event) {
+    setInput({
+      ...input,
+      [event.target.name]: event.target.value
+    });
+  }
+
+  if(action === 'show') {
+    return (
+      <div className={style.taskContainer}>
+        <h2 className={style.title}>{title}</h2>
+        <p>{description}</p>
+        <small>Assigned to: {assignedTo}</small>
+      </div>
+    )
+  } else if(action === 'create') {
+    return (
+      <div className={style.taskContainer}>
+        <input className={style.title} type="text" name="title" placeholder='Task title' onChange={(e) => handleChange(e)} />
+        <input className={style.description} type="text" name="description" placeholder='Task description' onChange={(e) => handleChange(e)} />
+        <button className='info' onClick={() => create(input)}>Create</button>
+      </div>
+    )
+  }
 }
