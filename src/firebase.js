f23c1a17e8e33036bb27ab7aa3a9c1e843b5f88b
lcenriquez <luiscarlos_enriquez@me.com>
2022-01-09 17:01:15 -0600

    Add production environment config

--- src/firebase.js
+++ src/firebase.js
@@ -2,16 +2,14 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
-const functions = require('firebase-functions');
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY || functions.config().firebase.api_key,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN || functions.config().auth_domain,
-  projectId: process.env.REACT_APP_PROJECT_ID || functions.config().firebase.project_id,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET || functions.config().firebase.storage_bucket,
-  messagingSenderId: process.env.REACT_APP_MS_ID || functions.config().firebase.ms_id,
-  appId: process.env.REACT_APP_APP_ID || functions.config().firebase.app_id
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MS_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 export const app = initializeApp(firebaseConfig);
